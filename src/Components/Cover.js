83c196c2a4a53fb8050c2f5308724490c3695761
Aarushi Shanker <aarushishanker3108@gmail.com>
2021-10-04 16:16:34 +0530

    Carousel fixed

--- src/Components/Cover.js
+++ src/Components/Cover.js
@@ -1,24 +1,21 @@
 import React,{useState} from 'react'
 import cover from '../images/cover.png'
 import {Carousel} from 'react-bootstrap'
+import './Cover.css'
 
 function Cover() {
-    const [index, setIndex] = useState(0);
-          
-    const handleSelect = (selectedIndex, e) => {
-       setIndex(selectedIndex);
-    };
+
           
     return (
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
+              <Carousel controls={false}>
+                <Carousel.Item interval={2000}>
                   <img
                     className="d-block w-100"
                     src={cover}
                     alt="First slide"
                   />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item interval={2000}>
                   <img
                     className="d-block w-100"
                     src={cover}
